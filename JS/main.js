8e5b52306ab1b9740cff9f33242083099da06f0e
Fymos <zufarovfayzullo703@gmail.com>
2023-12-14 18:54:16 +0500

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+alert("Welcome My Friend!".toUpperCase())
+var name = prompt("Please write your name".toUpperCase())
+var surname = prompt("Please write your surname".toUpperCase())
+var number = prompt("Please write your phone number".toUpperCase())
+console.log("Name: ", name);
+console.log("Surname: ", surname);
+console.log("Phone Number: ", number);
